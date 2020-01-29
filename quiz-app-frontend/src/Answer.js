f6d25ac16af5ca31f4fd681f7c2f414de61b88fd
conorbeamish <conorbeamish@hotmail.co.uk>
2020-01-29 17:24:14 +0000

    Refactor Score and Answer

--- quiz-app-frontend/src/Answer.js
+++ quiz-app-frontend/src/Answer.js
@@ -2,13 +2,14 @@ import React from "react";
 import he from 'he';
 
 const Answer = props => { 
+    const correctAnswer =  he.decode(props.correct_answer)
         if(props.correct){
             return(
-                <p>You are correct! {he.decode(props.correct_answer)}</p>
+                <p>You are correct! {correctAnswer}</p>
             )
         } else {
             return(
-                <p>Sorry the correct answer is: {he.decode(props.correct_answer)}</p>
+                <p>Sorry the correct answer is: {correctAnswer}</p>
             )
         }
     }
--- quiz-app-frontend/src/Score.js
+++ quiz-app-frontend/src/Score.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 const Score = props => {
+    const replay = <button onClick= {props.handleSubmit}>Play Again</button>
+
     if(props.questions.length === props.questionsAnswered){
         return(
             <div>
                 <p>You Scored {props.score}/{props.questions.length}</p>
-                <button onClick= {props.handleSubmit}>Play Again</button>
+                {replay}
             </div>
         )
     }else{
         return(
-            <button onClick= {props.handleSubmit}>Play Again</button>
+            replay
         )
     }
 
